5b810cab2f31f74391a3fd0a5f28c3b7222387c2
Chidike Henry <dk1online25@gmail.com>
2023-04-10 23:59:16 +0100

    added new feat

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,13 +3,15 @@ import Head from "next/head";
 
 interface LayoutProps {
   children: ReactNode;
+  title?: string;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, title = '' }: LayoutProps) {
   return (
     <>
       <Head>
-        <title>Glowgear e-commerce</title>
+        <title>{title ? title + ' - Glowgear e-commerce' : 'Glowgear e-commerce'}</title>
+        <meta name="description" content="Ecommerce Website"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
